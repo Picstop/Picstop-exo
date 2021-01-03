294ea67d2c1bc98bc48be3f28330bd90c0158db3
Jacob Trock <jtbosshku@gmail.com>
2021-01-03 15:50:49 -0500

    Update user.ts

--- models/user.ts
+++ models/user.ts
@@ -12,14 +12,30 @@ const UserSchema = new Schema({
     email: {
         type: String,
         required: true,
+        required: [true, "No email provided"],
+        unique: [true, "The email you provided was not unique"],
+        match: [
+          /^[^\s@]+@([^\s@.,]+\.)+[^\s@.,]{2,}$/,
+          "The email you provided was not correctly typed",
+        ],
     },
     username: {
         type: String,
         required: true,
+        required: [true, "No name provided"],
+        unique: [true, "The name you provided was not unique"],
+        minlength: [6, "name must be at least 6 characters"],
+        maxlength: [40, "name must be less than 40 characters"],
+        match:[
+            /^[a-zA-Z0-9_.]*$/,
+            "username is improperly formatted(must be only characters a-z,0-9,period and underscore)"
+        ]
     },
     name: {
         type: String,
         required: true,
+        minlength: [1, "name must be at least 6 characters"],
+        maxlength: [40, "name must be less than 40 characters"]
     },
     password: {
         type: String,
