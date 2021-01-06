975763e2632b7d6105a4c3ecb82b667171b9fb37
Jacob Trock <jtbosshku@gmail.com>
2021-01-06 12:29:23 -0500

    remove unneeded nexts

--- routes/posts.ts
+++ routes/posts.ts
@@ -4,14 +4,14 @@ import PostController from '../controllers/posts';
 const router = express.Router();
 const Posts = new PostController();
 
-router.post('/', async (req, res, next) => Posts.createPost(req, res, next));
+router.post('/', async (req, res) => Posts.createPost(req, res));
 
-router.get('/:id', async (req, res, next) => Posts.getPost(req, res, next));
+router.get('/:id', async (req, res) => Posts.getPost(req, res));
 
-router.delete('/:id', async (req, res, next) => Posts.deletePost(req, res, next));
+router.delete('/:id', async (req, res) => Posts.deletePost(req, res));
 
-router.get('/user', async (req, res, next) => Posts.getUserPosts(req, res, next));
+router.get('/user', async (req, res) => Posts.getUserPosts(req, res));
 
-router.patch('/:id', async (req, res, next) => Posts.updatePostCaption(req, res, next));
+router.patch('/:id', async (req, res) => Posts.updatePostCaption(req, res));
 
 export default router;
