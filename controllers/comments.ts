f678a0cf96bd85833be306e85838fef064f71730
Jacob Trock <jtbosshku@gmail.com>
2021-01-06 12:27:53 -0500

    switch json to unblock compiler

--- controllers/comments.ts
+++ controllers/comments.ts
@@ -51,7 +51,7 @@ export default class CommentController {
     async deleteComment(req: Request, res: Response, next: NextFunction) {
         const { id } = req.query;
         const commentAuthor = await Comment.findById(id).exec();
-        if (commentAuthor !== req.user.id) {
+        if (commentAuthor !== req.user['id']) {
             // TODO: add a custom error and a try/catch
             logger.error(`Error when deleting a comment ${id}: User is not author of post`);
             return res
@@ -70,7 +70,7 @@ export default class CommentController {
 
     likeComment(req: Request, res: Response, next: NextFunction) {
         const { commentId } = req.query;
-        return Comment.findByIdAndUpdate(commentId, { $push: { likes: req.user.id } })
+        return Comment.findByIdAndUpdate(commentId, { $push: { likes: req.user['id'] } })
             .exec()
             .then((result) => res.status(200).json({ success: true, message: result }))
             .catch((err) => {
