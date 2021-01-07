1c3417dc6bd2dbe9cbd3b5e6d6c4d936906d4eec
Jacob Trock <jtbosshku@gmail.com>
2021-01-06 20:19:54 -0500

    Update posts.ts

--- routes/posts.ts
+++ routes/posts.ts
@@ -4,14 +4,14 @@ import PostController from '../controllers/posts';
 const router = express.Router();
 const Posts = new PostController();
 
-router.post('/', async (req, res) => Posts.createPost(req, res));
+router.post('/createPost/', async (req, res) => Posts.createPost(req, res));
 
-router.get('/:id', async (req, res) => Posts.getPost(req, res));
+router.get('/getPost/:id', async (req, res) => Posts.getPost(req, res));
 
-router.delete('/:id', async (req, res) => Posts.deletePost(req, res));
+router.delete('/deletePost/:id', async (req, res) => Posts.deletePost(req, res));
 
-router.get('/user', async (req, res) => Posts.getUserPosts(req, res));
+router.get('/getUserPosts/', async (req, res) => Posts.getUserPosts(req, res));
 
-router.patch('/:id', async (req, res) => Posts.updatePostCaption(req, res));
+router.patch('/updatePostCaption/:id', async (req, res) => Posts.updatePostCaption(req, res));
 
 export default router;
