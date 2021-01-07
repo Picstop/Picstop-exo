1af853be6a0a2eefa563e12c30307d3c93af6a42
Alex Nikanov <alekseynikanov.21@gmail.com>
2021-01-07 19:54:39 +0200

    fix: :pencil2: Added missing imports to index.ts

--- index.ts
+++ index.ts
@@ -1,33 +1,37 @@
-import './config/passport'
+import './config/passport';
 
 import * as dotenv from 'dotenv';
 import morgan from 'morgan';
 import passport from 'passport';
 import express from 'express';
 import bodyParser from 'body-parser';
+import Redis from 'ioredis';
+import connectRedis from 'connect-redis';
 
-import * as userController from './controllers/user';
+import session from 'express-session';
 import db from './database/database';
 import initLogger from './core/logger';
 import locationRoutes from './routes/locations';
 
+import userRoutes from './routes/users';
+
 dotenv.config();
 
-const RedisStore = connectRedis(session)
+const RedisStore = connectRedis(session);
 const client = new Redis({
     port: Number(process.env.REDIS_PORT),
     host: process.env.REDIS_HOST,
-    password: process.env.REDIS_PASSWORD
-})
+    password: process.env.REDIS_PASSWORD,
+});
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 const logger = initLogger('index');
 
-client.on('connect', () =>{
-    logger.info('Connected to Redis')
-})
+client.on('connect', () => {
+    logger.info('Connected to Redis');
+});
 
 app.use(session({
     store: new RedisStore({
@@ -36,7 +40,7 @@ app.use(session({
         cookie: {
             maxAge: Number(process.env.REDIS_AGE),
             secure: process.env.NODE_ENV === 'production',
-            sameSite: true
+            sameSite: true,
 
         },
     }),
@@ -44,7 +48,7 @@ app.use(session({
     secret: process.env.REDIS_SECRET,
     resave: false,
 
-}))
+}));
 
 app.use(passport.initialize());
 app.use(passport.session());
