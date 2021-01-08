dba5958eef081359a8581fabb22b7662925def2f
Jacob Trock <jtbosshku@gmail.com>
2021-01-08 18:04:21 -0500

    Update users.ts

--- middleware/users.ts
+++ middleware/users.ts
@@ -9,139 +9,146 @@ export default class UserMiddleware {
         if (!req.body.username || !req.body.email || !req.body.password || !req.body.password2) {
             return res.status(400).json({ success: false, message: 'Missing parameters' });
         }
-        next();
+        return next();
     }
 
     checkPasswordMatch(req: Request, res: Response, next: NextFunction) {
         const { password, password2 } = req.body;
-        if (password === password2) {
-            next();
-        } else if (password !== password2) {
-            return res.status(400).json({ success: false, message: 'Passwords do not match' });
-        }
+        if (password === password2) { return next(); }
+        return res.status(400).json({ success: false, message: 'Passwords do not match' });
     }
 
     async allowedToViewProfile(req: Request, res: Response, next: NextFunction) {
-        const { username } = req.params;
-        const user = await User.findOne({ username }).exec();
+        const { id } = req.params;
+        const user = await User.findById(id)
+            .orFail(new Error('User not found!'))
+            .exec();
         const follows = user.followers.some((follower) => `${follower}` === (req.user._id));
-        const incomingBlocks = await user.blocked.some((user) => `${user}` == (req.user._id));
-        const outgoingBlocks = await req.user.blocked.some((users) => `${users}` == user._id);
-        if (incomingBlocks == true || outgoingBlocks == true) {
+        const incomingBlocks = await user.blocked.some((usr) => `${usr}` === (req.user._id));
+        const outgoingBlocks = await req.user.blocked.some((users) => `${users}` === user._id);
+        if (incomingBlocks === true || outgoingBlocks === true) {
             return res.status(401).json({ success: false, message: 'User either blocked you or you blocked user' });
         }
-        if (!user.private) {
-            next();
-        } else {
-            if (!follows) {
-                return res.status(200).json({
-                    success: true,
-                    private: true,
-                    message: {
-                        _id: user._id,
-                        followers: user.followers.length,
-                        following: user.following.length,
-                        name: user.name,
-                        bio: user.bio,
-                    },
-                });
-            } if (follows) {
-                next();
-            }
+        if (!user.private) { return next(); }
+
+        if (!follows) {
+            return res.status(200).json({
+                success: true,
+                private: true,
+                message: {
+                    _id: user._id,
+                    followers: user.followers.length,
+                    following: user.following.length,
+                    name: user.name,
+                    bio: user.bio,
+                },
+            });
         }
+        return next();
     }
 
     // isBlocked
     async isBlocked(req: Request, res: Response, next: NextFunction) {
         const { id } = req.body;
-        const user = await User.findById(id).exec();
-        const incomingBlocks = await user.blocked.some((user) => `${user}` == (req.user._id));
-        const outgoingBlocks = await req.user.blocked.some((users) => `${users}` == user._id);
-        if (incomingBlocks == true || outgoingBlocks == true) {
+        const user = await User.findById(id)
+            .orFail(new Error('User not found!'))
+            .exec();
+        const incomingBlocks = await user.blocked.some((usr) => `${usr}` === (req.user._id));
+        const outgoingBlocks = await req.user.blocked.some((users) => `${users}` === user._id);
+        if (incomingBlocks === true || outgoingBlocks === true) {
             return res.status(401).json({ success: false, message: 'User either blocked you or you blocked user' });
         }
-        next();
+        return next();
     }
 
     async alreadyBlocked(req: Request, res: Response, next: NextFunction) {
         const { id } = req.body;
-        const user = await User.findById(id).exec();
-        const outgoingBlocks = await req.user.blocked.some((users) => `${users}` == user._id);
-        if (outgoingBlocks == true) {
+        const user = await User.findById(id)
+            .orFail(new Error('User not found!'))
+            .exec();
+        const outgoingBlocks = await req.user.blocked.some((users) => `${users}` === user._id);
+        if (outgoingBlocks === true) {
             return res.status(401).json({ success: false, message: 'User already blocked' });
         }
-        next();
+        return next();
     }
 
     async alreadyFollowedOrRequested(req: Request, res: Response, next: NextFunction) {
         const { id } = req.body;
-        const user = await User.findById(id).exec();
-        const follows = user.followers.some((follower) => `${follower}` == (req.user._id));
-        const requested = user.followerRequests.some((follower) => `${follower}` == (req.user._id));
-        if (follows == true || requested == true) {
+        const user = await User.findById(id)
+            .orFail(new Error('User not found!'))
+            .exec();
+        const follows = user.followers.some((follower) => `${follower}` === (req.user._id));
+        const requested = user.followerRequests.some((follower) => `${follower}` === (req.user._id));
+        if (follows === true || requested === true) {
             return res.status(401).json({ success: false, message: 'User already requested or follows user' });
         }
-        next();
+        return next();
     }
 
     // check if already unblocked
     async alreadyUnblocked(req: Request, res: Response, next: NextFunction) {
         const { id } = req.body;
-        const user = await User.findById(id).exec();
-        const blocked = req.user.blocked.some((users) => `${users}` == user._id);
+        const user = await User.findById(id)
+            .orFail(new Error('User not found!'))
+            .exec();
+        const blocked = req.user.blocked.some((users) => `${users}` === user._id);
 
         if (!blocked) {
             return res.status(400).json({ success: false, message: 'User is already unblocked' });
         }
-        next();
+        return next();
     }
 
     // check if already unfollowed
     async alreadyUnfollowed(req: Request, res: Response, next: NextFunction) {
         const { id } = req.body;
-        const user = await User.findById(id).exec();
-        const follows = user.followers.some((follower) => `${follower}` == (req.user._id));
+        const user = await User.findById(id)
+            .orFail(new Error('User not found!'))
+            .exec();
+        const follows = user.followers.some((follower) => `${follower}` === (req.user._id));
         if (!follows) {
             return res.status(400).json({ success: false, message: 'User is already unfollowed' });
         }
-        next();
+        return next();
     }
 
     // check if follow request exists
     async alreadyRequestedToFollow(req: Request, res: Response, next: NextFunction) {
         const { id } = req.body;
         const user = await User.findById(id).exec();
-        const requested = user.followerRequests.some((follower) => `${follower}` == (req.user._id));
-        if (!requested) {
-            next();
-        } else {
-            return res.status(400).json({ success: false, message: 'Already requested to follow' });
-        }
+        const requested = user.followerRequests.some((follower) => `${follower}` === (req.user._id));
+        if (!requested) { return next(); }
+        return res.status(400).json({ success: false, message: 'Already requested to follow' });
     }
 
     async didntRequestToFollow(req: Request, res: Response, next: NextFunction) {
         const { id } = req.body;
-        const user = await User.findById(id).exec();
-        const requested = user.followerRequests.some((follower) => `${follower}` == (req.user._id));
+        const user = await User.findById(id)
+            .orFail(new Error('User not found!'))
+            .exec();
+        const requested = user.followerRequests.some((follower) => `${follower}` === (req.user._id));
         if (!requested) {
             return res.status(400).json({ success: false, message: 'Follow request does not exist.' });
         }
-        next();
+        return next();
     }
 
     async followRequestExists(req: Request, res: Response, next: NextFunction) {
         const { id } = req.body;
-        const user = await User.findById(id).exec();
-        const requested = req.user.followerRequests.some((follower) => `${follower}` == (user.id));
+        const user = await User.findById(id)
+            .orFail(new Error('User not found!'))
+            .exec();
+        const requested = req.user.followerRequests.some((follower) => `${follower}` === (user.id));
         if (!requested) {
             return res.status(400).json({ success: false, message: 'Follow request does not exist.' });
         }
-        next();
+        return next();
     }
 
     async notThemself(req: Request, res: Response, next: NextFunction) {
         const { id } = req.body;
-        if (req.user._id == id) return res.status(400).json({ success: false, message: 'Cannot attempt user actions on yourself' });
-        next();
+        if (req.user._id === id) return res.status(400).json({ success: false, message: 'Cannot attempt user actions on yourself' });
+        return next();
     }
 }
