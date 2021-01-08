87be3e6651b444d1568f733b408ec87416b81a23
Jacob Trock <jtbosshku@gmail.com>
2021-01-08 10:45:54 -0500

    Update posts.ts

--- controllers/posts.ts
+++ controllers/posts.ts
@@ -7,8 +7,14 @@ import { loggers } from 'winston';
 import Post from '../models/post';
 import { Post as PostType } from '../types/types';
 import initLogger from '../core/logger';
+import User from '../models/user';
+import Redis from 'ioredis'
 
 const logger = initLogger('ControllerPosts');
+
+const client = Redis.createClient(process.env.REDIS_PORT);
+
+client.on("error", (err) => logger.error(`Redis Error: ${err}`));
 
 export default class PostController {
     createPost(req: Request, res: Response, next: NextFunction) {
@@ -73,9 +79,9 @@ export default class PostController {
     }
 
     async updatePostCaption(req: Request, res: Response, next: NextFunction) {
-        const authorId = Post.find({ authorId: req.user.id });
+        const authorId = Post.find({ authorId: req.user['id'] });
         const { caption, postId } = req.body;
-        if (authorId !== req.user.id) {
+        if (authorId !== req.user['id']) {
             // TODO: raise and try/catch a custom error here
             logger.info('Error when updating post caption: User is not an author of post');
             return res.status(401).json({ success: false, message: 'User is not author of post.' });
@@ -84,6 +90,53 @@ export default class PostController {
             .then((result) => res.status(200).json({ success: true, message: result }))
             .catch((err) => {
                 logger.error(`Error when updating post ${postId}: ${err}`);
+                return res.status(500).json({ success: false, message: err });
+            });
+    }
+
+    async getFeedSet(req: Request, res: Response) {
+        const { userId } = req.body;
+        const getStr = `SeenPosts:${userId}`;
+        const PostList = await client.get(getStr).split(",")||[];
+
+        User.findById(userId).exec()
+            .then(usr => Post.find({
+                _id: {
+                    $ne: {
+                        $in: PostList
+                    }
+                },
+                authorId: {
+                    $in: usr.following
+                }
+            }).limit(20))
+            .then(posts => new Promise((resolve, reject) => {
+                posts.forEach(p => PostList.push(p['_id']))
+                client.setex(getStr, 600, PostList.toString())
+                    .then(() => resolve(posts))
+                    .catch(err => reject(err))
+            }))
+            .then((result) => res.status(200).json({
+                success: true,
+                message: result,
+            }))
+            // .then(async (result: any) => { //switch me in when aws upload is merged
+            //     const orderPromises = result.map((i) => this.getDownload(i));
+            //     return Promise.all(orderPromises)
+            //         .then((urls) => res.status(200).json({
+            //             success: true,
+            //             message: {
+            //                 posts: result,
+            //                 url: urls,
+            //             },
+            //         }))
+            //         .catch((err) => {
+            //             logger.error(`Error when acquiring feed for user ${userId}: ${err}`);
+            //             return res.status(500).json({ success: false, message: err });
+            //         });
+            // })
+            .catch((err) => {
+                logger.error(`Error when acquiring feed for user ${userId}: ${err}`);
                 return res.status(500).json({ success: false, message: err });
             });
     }
