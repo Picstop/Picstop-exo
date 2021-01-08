52fcdd64fe30c61d03468adc843724c57f34a7e9
Jacob Trock <jtbosshku@gmail.com>
2021-01-08 17:58:38 -0500

    Update passport.ts

--- config/passport.ts
+++ config/passport.ts
@@ -9,20 +9,25 @@ passport.serializeUser((user: IUser, done) => {
 });
 
 passport.deserializeUser((id, done) => {
-    User.findById(id, (err : Error, user : IUser) => {
-        done(err, user);
-    });
+    User.findById(id)
+        .orFail(new Error('User not found!'))
+        .exec((err: Error, user: IUser) => {
+            if (err) return done(err, null);
+            return done(null, user);
+        });
 });
 
 passport.use(
     new LocalStrategy({ usernameField: 'username' }, (username, password, done) => {
-        User.findOne({ username }).select('+password').exec()
+        User.findOne({ username }).select('+password')
+            .orFail(new Error('User not found!'))
+            .exec()
             .then((user: IUser) => {
                 if (!user) {
                     return done(null, false, { message: `Username ${username} not found.` });
                 }
 
-                user.comparePassword(password, user.password, (err: Error, isMatch: boolean) => {
+                return user.comparePassword(password, user.password, (err: Error, isMatch: boolean) => {
                     if (err) { return done(err); }
                     if (isMatch) {
                         return done(undefined, user);
@@ -55,5 +60,5 @@ export const isAuthenticated = (req: Request, res: Response, next: NextFunction)
     if (req.isAuthenticated()) {
         return next();
     }
-    res.status(401).json({ message: 'Not logged in' });
+    return res.status(401).json({ message: 'Not logged in' });
 };
