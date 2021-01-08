758a5e8fc2a147c14b3c923c5179b9c2f3671c2a
Alex Nikanov <alekseynikanov.21@gmail.com>
2021-01-08 15:42:07 +0200

    feat: :sparkles: Added CommentMiddleware method that verifies authorship for the comment in request

---
+++ middleware/comments.ts
@@ -0,0 +1,45 @@
+import mongoose from 'mongoose';
+import { NextFunction, Request, Response } from 'express'
+
+import Comment from '../models/comment';
+import initLogger from '../core/logger';
+
+const logger = initLogger('MiddlewareComments');
+
+export default class CommentMiddleware {
+    /**
+     * Verifies is the `authorId` in the request is the same as the authorId for the comment the request is trying to access
+     * @param {express.Request} req Request object
+     * @param {express.Response} res Response object
+     * @param {express.NextFunction} next The next function to call if this one passes
+     */
+    async verifyAuthor(req: Request, res: Response, next: NextFunction) {
+        const { id } = req.body;
+        const currUserId = req.user['_id'];
+
+        // Verify first that this comment exists in the database
+        // TODO: #10 add this logging to the controller and use the controller method here instead
+        let comment;
+        try {
+            comment = await Comment.findById(id);
+        } catch (e) {
+            if (e instanceof mongoose.Error.DocumentNotFoundError) {
+                logger.info(`Document with id ${id} not found.`);
+                return res
+                    .status(400)
+                    .json({ success: false, message: 'Comment was not found' });
+            } else {
+                throw e;
+            }
+        }
+
+        // Note: use .equals instead of ==
+        if (comment.authorId.equals(currUserId)) {
+            next();
+        } else {
+            return res
+                .status(500)
+                .json({ success: false, message: 'Author id does not match. Access forbidden.' });
+        }
+    }
+}
