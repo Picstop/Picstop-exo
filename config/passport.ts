4525f546edfb688bde40af23364c1695e5d01c3e
Jacob Trock <jtbosshku@gmail.com>
2021-01-20 18:51:28 -0500

    Update passport.ts

--- config/passport.ts
+++ config/passport.ts
@@ -3,19 +3,20 @@
 import { Request, Response, NextFunction } from 'express';
 import jwt from 'jsonwebtoken';
 import User from '../models/user';
+import { IUser } from '../types/types';
 
 export const isAuthenticated = (req: Request, res: Response, next: NextFunction) => {
     const authHeader = req.headers.authorization;
     if (authHeader) {
         jwt.verify(authHeader, process.env.JWT_SECRET || '$$2d##dS#', (err, out) => {
             if (err)res.status(400).json({ success: false, message: err.message });
-            User.findById(out._id,)
-                .then((ex: Boolean) => {
-                if (ex) {
-                    req.user = out;
-                    next();
-                } else return res.status(401).json({ success: false, message: 'Unauthenticated' });
-            })
+            User.findById(out._id)
+                .then((ex: IUser) => {
+                    if (ex) {
+                        req.user = out;
+                        return next();
+                    } return res.status(401).json({ success: false, message: 'Unauthenticated' });
+                })
                 .catch((e) => next(e));
         });
     } else {
